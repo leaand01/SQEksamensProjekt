b58e08c88c3895599b2c41a2684f9d66d8e56508
leaand01 <leaand01@easv365.dk>
2024-05-30 23:03:09 +0200

    added note to BankFees.py

--- MVC/Models_DTO/PrincipalController.py
+++ MVC/Models_DTO/PrincipalController.py
@@ -7,8 +7,3 @@ local_db = LocalDB(dict_banks)
 banks = Banks(local_db)
 
 principal = Principal(banks=banks, processor=ProcessUserInputs())
-
-
-# For debugging:
-# user_inputs = {'house_price': '1.000.000', 'down_payment': '50.000', 'bond_price': '200', 'dropdown': 'Totalkredit'}
-# principal.calculate(user_inputs)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -29,5 +30,8 @@ async def welcome_page_with_calc_result(request: Request):
                                    "principal_value": principal.principal,
                                    "capital_loss": principal.capital_loss
                                    })
-
 # Remark: According to .net MVC setup this corresponds to a Controller, since handles html requests.
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
